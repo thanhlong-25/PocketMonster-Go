4116b1532b3c1aa27e7e8eafd8cc420f5de147ba
ThanhLong <long.tranthanh025@gmail.com>
2023-12-15 00:45:07 +0700

    update SpriteAnimator class

--- Assets/Scripts/Character/NpcController.cs
+++ Assets/Scripts/Character/NpcController.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class NpcController : MonoBehaviour, Interactable {
     [SerializeField] Dialog dialogBox;
+    [SerializeField] List<Sprite> sprites;
+
+    SpriteAnimator spriteAnimator;
+
+    private void Start() {
+        spriteAnimator = new SpriteAnimator(sprites, GetComponent<SpriteRenderer>());
+        spriteAnimator.Start();
+    }
+
+    private void HandleUpdate() {
+        spriteAnimator.HandleUpdate();
+    }
 
     public void Interact() {
         StartCoroutine(DialogController.Instance.ShowDialog(dialogBox));
